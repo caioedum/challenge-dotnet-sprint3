75438e8839309e6922d1526160d6ea40ec0f4a27
Caio Eduardo <caio3martins@gmail.com>
2025-03-16 20:50:07 -0300

    feat: add dbcontext class

---
+++ WebApiChallenge/Context/WebApiDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiChallenge.Models;
+
+namespace WebApiChallenge.Context
+{
+    public class WebApiDbContext : DbContext
+    {
+        public WebApiDbContext(DbContextOptions<WebApiDbContext> options) : base(options)
+        {
+        }
+        public DbSet<AtendimentoUsuario> AtendimentosUsuarios { get; set; }
+        public DbSet<Clinica> Clinicas { get; set; }
+        public DbSet<ContatoUsuario> ContatosUsuarios { get; set; }
+        public DbSet<Dentista> Dentistas { get; set; }
+        public DbSet<EnderecoClinica> EnderecosClinicas { get; set; }
+        public DbSet<EnderecoUsuario> EnderecosUsuarios { get; set; }
+        public DbSet<ImagemUsuario> ImagensUsuarios { get; set; }
+        public DbSet<PrevisaoUsuario> PrevisoesUsuarios { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
