e36248e6fab372cc627f3ce55c7d7cf4afcbcc64
Caio Eduardo <caio3martins@gmail.com>
2025-03-19 00:05:19 -0300

    remove: delete migrations and dto

--- WebApiChallenge/Context/WebApiDbContext.cs
+++ WebApiChallenge/Context/WebApiDbContext.cs
@@ -18,56 +18,5 @@ namespace WebApiChallenge.Context
         public DbSet<PrevisaoUsuario> PrevisoesUsuarios { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Usuario>()
-                .HasIndex(u => u.Cpf)
-                .IsUnique();
-
-            modelBuilder.Entity<ContatoUsuario>()
-                .HasIndex(c => c.Email)
-                .IsUnique();
-
-            modelBuilder.Entity<Usuario>()
-                .HasMany(u => u.Contatos)
-                .WithOne(c => c.Usuario)
-                .HasForeignKey(c => c.UsuarioId);
-
-            modelBuilder.Entity<Usuario>()
-                .HasMany(u => u.Enderecos)
-                .WithOne(e => e.Usuario)
-                .HasForeignKey(e => e.UsuarioId);
-
-            modelBuilder.Entity<Usuario>()
-                .HasMany(u => u.Imagens)
-                .WithOne(i => i.Usuario)
-                .HasForeignKey(i => i.UsuarioId);
-
-            modelBuilder.Entity<Usuario>()
-                .HasMany(u => u.Atendimentos)
-                .WithOne(a => a.Usuario)
-                .HasForeignKey(a => a.UsuarioId);
-
-            modelBuilder.Entity<Usuario>()
-                .HasOne(u => u.Dentista)
-                .WithOne(d => d.Usuario)
-                .HasForeignKey<Dentista>(d => d.UsuarioId);
-
-            modelBuilder.Entity<Dentista>()
-                .HasMany(d => d.Clinicas)
-                .WithOne(c => c.Dentista)
-                .HasForeignKey(c => c.DentistaId);
-
-            modelBuilder.Entity<Clinica>()
-                .HasMany(c => c.EnderecosClinica)
-                .WithOne(e => e.Clinica)
-                .HasForeignKey(e => e.ClinicaId);
-
-            modelBuilder.Entity<ImagemUsuario>()
-                .HasMany(i => i.PrevisoesUsuarios)
-                .WithOne(p => p.ImagemUsuario)
-                .HasForeignKey(p => p.ImagemUsuarioId);
-        }
-
     }
 }
